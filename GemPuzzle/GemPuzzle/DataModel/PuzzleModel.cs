96a05ef21419285c1c8605a2e1d5909368147821
Ilya Petrov <petrik626@gmail.com>
2018-05-27 18:31:29 +0300

    The data model and markup have been added in project. The animation has been added in markup

---
+++ GemPuzzle/GemPuzzle/DataModel/PuzzleModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GemPuzzle.DataModel
+{
+    internal class PuzzleModel:UIElement
+    {
+        #region FIELDS
+        private static Lazy<PuzzleModel> instance;
+        public static DependencyProperty ActualRowIndexProperty;
+        public static DependencyProperty ActualColumnIndexProperty;
+        public static DependencyProperty MoveRowIndexProperty;
+        public static DependencyProperty MoveColumnIndexProperty;
+        #endregion
+        #region CONSTRUCTORS
+        private PuzzleModel() { }
+        static PuzzleModel()
+        {
+            instance = new Lazy<PuzzleModel>(() => new PuzzleModel());
+
+            FrameworkPropertyMetadata metadataActualRow = new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault);
+            FrameworkPropertyMetadata metadataActualColumn = new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault);
+            FrameworkPropertyMetadata metadataMoveRow = new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault);
+            FrameworkPropertyMetadata metadataMoveColumn = new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault);
+
+
+            ActualRowIndexProperty = DependencyProperty.Register("ActualRowIndex", typeof(int), typeof(PuzzleModel), metadataActualRow);
+            ActualColumnIndexProperty = DependencyProperty.Register("ActualColumnIndex", typeof(int), typeof(PuzzleModel), metadataActualColumn);
+            MoveRowIndexProperty = DependencyProperty.Register("MoveRowIndex", typeof(int), typeof(PuzzleModel), metadataMoveRow);
+            MoveColumnIndexProperty = DependencyProperty.Register("MoveColumnIndex", typeof(int), typeof(PuzzleModel), metadataMoveColumn);
+        }
+        #endregion
+        #region PROPERTIES
+        public static PuzzleModel Source => instance.Value;
+        public int ActualRowIndex
+        {
+            get => (int)GetValue(ActualRowIndexProperty);
+            set => SetValue(ActualRowIndexProperty, value);
+        }
+        public int ActualColumnIndex
+        {
+            get => (int)GetValue(ActualColumnIndexProperty);
+            set => SetValue(ActualColumnIndexProperty, value);
+        }
+        public int MoveRowIndex
+        {
+            get => (int)GetValue(MoveRowIndexProperty);
+            set => SetValue(MoveRowIndexProperty, value);
+        }
+        public int MoveColumnIndex
+        {
+            get => (int)GetValue(MoveColumnIndexProperty);
+            set => SetValue(MoveColumnIndexProperty, value);
+        }
+        #endregion
+    }
+}
---
+++ GemPuzzle/GemPuzzle/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GemPuzzle.DataModel;
+
+namespace GemPuzzle
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PuzzleModel model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = PuzzleModel.Source;
+            this.DataContext = model;
+        }
+
+        private void Button15_Click(object sender, RoutedEventArgs e)
+        {
+            //Blanket
+            model.ActualRowIndex = 3;
+            model.ActualColumnIndex = 2;
+            model.MoveRowIndex = 3;
+            model.MoveColumnIndex = 3;
+        }
+    }
+}
